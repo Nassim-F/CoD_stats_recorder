128c4bd822319cbe47e2bc1477caa069c0ff13ff
Nassim <na.faltakh@gmail.com>
2025-06-25 15:00:43 +0200

    update script with code quality check (isort, pylint and black)

--- ocr_game_stats.py
+++ ocr_game_stats.py
@@ -1,11 +1,13 @@
-from datetime import datetime
-import easyocr
+# pylint: disable=missing-module-docstring
 
+from datetime import datetime
+
+import easyocr
 
 start_time = datetime.now()
 
-reader = easyocr.Reader(['fr'])  # Spécifier la langue
-result = reader.readtext('test_image/IMG_4632.JPG')
+reader = easyocr.Reader(["fr"])  # Spécifier la langue
+result = reader.readtext("test_image/IMG_4632.JPG")
 
 text_result = [detection[1] for detection in result]
 
@@ -15,5 +17,4 @@ print(len(text_result))
 
 end_time = datetime.now()
 
-print('Duration: {}'.format(end_time - start_time))
-
+print(f"Duration: {format(end_time - start_time)}")
