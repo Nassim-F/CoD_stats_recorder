e2c92c22648342bac18d9b2cbba577cde9af518c
Nassim <na.faltakh@gmail.com>
2025-06-26 15:58:39 +0200

    Add 2 functions in order to split the image and separate where we extract map/mode name and the score board + update gitignore

--- ocr_game_stats.py
+++ ocr_game_stats.py
@@ -2,19 +2,60 @@
 
 from datetime import datetime
 
+import cv2
 import easyocr
 
 start_time = datetime.now()
 
-reader = easyocr.Reader(["fr"])  # Spécifier la langue
-result = reader.readtext("test_image/IMG_4632.JPG")
 
-text_result = [detection[1] for detection in result]
+def get_map_and_mode_name(path_image: str) -> dict:
+    """
+    Process the score board image in order to take only the map name and the game
+    :param path_image : image location to analyse
+    :return dict_game_mode_name : dictionary that contains the map name and the mode name
+    """
+    img = cv2.imread(path_image)
 
-print(text_result)
-print(len(text_result))
+    # Cropping image : 1st image to get the game mode and the map name and the second for the stats
+    game_mode = img[80:220, 310:1400]
 
+    reader = easyocr.Reader(["fr"])
+    result_game_mode = reader.readtext(game_mode)
+
+    text_result_game_mode = [detection[1] for detection in result_game_mode]
+
+    dict_game_mode_name = {
+        "map": text_result_game_mode[0],
+        "game_mode": text_result_game_mode[1],
+    }
+
+    return dict_game_mode_name
+
+
+def get_list_game_information(path_image: str) -> list:
+    """
+    Split the image to get the scoreboard part only
+    Then it stores all text information detected by easyocr
+    :param path_image:
+    :return: list
+    """
+    img = cv2.imread(path_image)
+
+    # Cropping image : 1st image to get the game mode and the map name and the second for the stats
+    score_board = img[360:2050, 370:3470]
+
+    reader = easyocr.Reader(["fr"])
+    result_score_board = reader.readtext(score_board)
+
+    list_game_information = [detection[1] for detection in result_score_board]
+
+    return list_game_information
+
+
+map_and_ode_name = get_map_and_mode_name("test_image/ec_win.png")
+list_game_info = get_list_game_information("test_image/ec_win.png")
 
 end_time = datetime.now()
 
-print(f"Duration: {format(end_time - start_time)}")
+print(map_and_ode_name)
+print(list_game_info)
